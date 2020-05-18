1ce27e0b2906a0c6e73165cb2e8e920f310ddf25
a-taniguchi <a.taniguchi@em.ci.ritsumei.ac.jp>
2020-05-18 17:15:21 +0900

    Done: spatial concept visualizer with topological graph

--- albert-b/learning/SpCoVisualizer.py
+++ albert-b/learning/SpCoVisualizer.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Spatial concept Topometric Map Visualizaer (Python only w/o ROS)
 # 場所概念の位置分布（ガウス分布）および、その隣接関係のグラフ（遷移確率）を地図上に描画する
-# Akira Taniguchi 2020/05/16- 
+# Akira Taniguchi 2020/05/16 - 2020/05/18
 # This code is based on em_spcoae_map_srv.py
 
 import sys
@@ -11,8 +11,9 @@ import scipy.stats as ss
 import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib import patheffects as path_effects
 from matplotlib.patches import Ellipse
-from PIL import Image,ImageOps
+from PIL import Image,ImageOps, ImageDraw
 import yaml
 from __init__ import *
 
@@ -29,6 +30,7 @@ with open(datasetfolder + map_file + '.yaml') as file:
 
 print((origin,resolution))
 
+# For the map on albert-B dataset
 x_min = 380 #X_init_index[0] - T_horizon
 x_max = 800 #X_init_index[0] + T_horizon
 y_min = 180 #X_init_index[1] - T_horizon
@@ -90,17 +92,49 @@ for k in xrange(K):
         ax.add_patch(el)
         
         # 中心点の描画
-        el2                  = Ellipse(xy=Mu_origin[k],width=1.0,height=1.0,color=COLOR[k])  # matplotlib.patches.Ellipse
-        ax.add_patch(el2)
+        plt.scatter(Mu_origin[k][0], Mu_origin[k][1], s=5,color=COLOR[k], zorder=2)
+        #el2                  = Ellipse(xy=Mu_origin[k],width=1.0,height=1.0,color=COLOR[k])  # matplotlib.patches.Ellipse
+        #ax.add_patch(el2)
+
+        txt = ax.text(Mu_origin[k][0]+4, Mu_origin[k][1]+4, str(k), size = 7, color = "midnightblue", zorder=3)
+        txt.set_path_effects([path_effects.Stroke(linewidth=2, foreground='white', alpha=0.8),
+                              path_effects.Normal()])
     except:
         break
 
 # グラフノードと遷移確率の描画
+edge_list = []
+psi_sym = (psi + psi.T) / 2.0 
+#psi_sym = psi_sym / np.sum(psi_sym,0)
+#print psi_sym,np.sum(psi_sym[0])
+ignore_value = 1.0 / K
+for k1 in range(K):
+    if list(It).count(k1) == 0:
+        continue
+    for k2 in range(K):
+        if list(It).count(k2) == 0:
+          continue
+        if (psi_sym[k1][k2] > ignore_value) and (k1 != k2) and (psi_sym[k1][k2] != 1.0):
+          if ( (k2,k1) not in edge_list ):
+              edge_list += [(k1,k2)]
+              print k1,k2,psi_sym[k1][k2]
+print edge_list
 
 
 # 地図描画
 plt.imshow(map_image,cmap='gray')
 #,extent=(origin[0],origin[0]+height*resolution,origin[1],origin[1]+width*resolution)
+
+for edge in edge_list:
+    plt.plot( (Mu_origin[edge[0]][0], Mu_origin[edge[1]][0]), (Mu_origin[edge[0]][1], Mu_origin[edge[1]][1]),
+              color='dimgray', linestyle = "-", linewidth=10*psi_sym[edge[0]][edge[1]], zorder=1 ) #, alpha=10*psi_sym[edge[0]][edge[1]]) #psi_sym[edge[0]][edge[1]])
+#darkslategrey
+
+##axの場合
+#ax.set_axisbelow(True)
+
+##rcParamsの場合
+#plt.rcParams['axes.axisbelow'] = True
 
 plt.tick_params(axis='x', which='major', labelsize=8)
 plt.tick_params(axis='y', which='major', labelsize=8)
@@ -111,176 +145,8 @@ plt.ylim(y_min,y_max)
 plt.savefig(file_trialname + '_A_SpCoGraph_' + iteration_sample + '.png', dpi=300)
 plt.savefig(file_trialname + '_A_SpCoGraph_' + iteration_sample + '.pdf', dpi=300)
 
-plt.show()
+#plt.show()
 
-#plt.cla()
-#plt.clf()
-#plt.close()
-
-
-
-################################################################################################
-"""
-map_callback関数でmapのtopicをsubscribeして探索候補点を算出し、変数で保持。
-map_server関数でROSサービスとして返す。
-
-
-#from __init__ import *
-#from em_spco_ae.srv import *
-
-class Map(object):
-    def map_callback(self, msg):
-        self.msg = msg
-        info = msg.info
-        data = msg.data
-
-        height       = info.height
-        width        = info.width
-        resolution   = np.round(info.resolution,decimals=3)
-        origin       = info.origin
-
-        #探索候補点の間隔（m）
-        interval     = 0.8
-        #何m四方内に占有セルがある場合に探索候補点から取り除くか
-        square       = 0.5
-        around       = int(round((square-resolution)/resolution)) / 2
-        #探索候補点のindex格納リスト
-        self.free_list    = list()
-        width_list   = range(0,width,int(interval/resolution))
-        height_list  = range(0,height,int(interval/resolution))
-        for y in width_list:
-            for x in height_list:
-                if data[y * height + x] == 0:
-                    free = True
-                    #50cm四方内に占有セルがある場合は候補から除去
-                    for y_ in range(y-around,y+around+1,1):
-                        for x_ in range(x-around,x+around+1,1):
-                            if data[y_ * height + x_] == 100:
-                                free = False
-                    if free:
-                        #探索候補点のmapトピック上のindexをリストに格納
-                        self.free_list.append(y * height + x)
-
-        #ROSサービスで返すためにMultiArrayに変換
-        self.free_array = Int64MultiArray()
-        self.free_array.data = self.free_list
-
-
-        #以下は、真の位置分布を描画する処理#########################################
-
-        #ディレクトリ作成と真のパラメータコピー
-        subprocess.Popen('mkdir -p ' + data_path + '/' + self.map_file, shell=True)
-        param        = parameter_copy(self.map_file)
-        _mu_k        = param[3]
-        _sigma_k     = param[4]
-
-        #分散共分散行列描画
-        fig  = plt.figure()
-        ax   = fig.add_subplot(1,1,1)
-        for k in xrange(K):
-            try:
-                lmda, vec            = np.linalg.eig(_sigma_k[k])
-                el_width,el_height   = 2 * el_c * np.sqrt(lmda)
-                el_angle             = np.rad2deg(np.arctan2(vec[1,0],vec[0,0]))
-                el                   = Ellipse(xy=_mu_k[k],width=el_width,height=el_height,angle=el_angle,color=colorlist[k],alpha=0.3)
-                ax.add_patch(el)
-            except:
-                break
-
-        #探索候補点プロット
-        for xy in xrange(len(self.free_list)):
-            #探索点のindex→探索点の座標
-            point        = index2point(xy,self.free_list,info)
-            point_pdf    = np.zeros(K)
-            for k in xrange(K):
-                try:
-                    point_pdf[k] = ss.multivariate_normal(mean=_mu_k[k],cov=_sigma_k[k]).pdf(np.array([point.x,point.y]))
-                except:
-                    break
-            index_i = np.argmax(point_pdf)
-            plt.plot(point.x,point.y,color=colorlist[index_i],marker='o',markersize=4)
-
-        #地図描画
-        map_file_path    = roslib.packages.get_pkg_dir('em_spco_ae') + '/map/' + self.map_file + '/map.pgm'
-        map_image        = Image.open(map_file_path)
-        plt.imshow(map_image,extent=(origin.position.x,origin.position.x+height*resolution,origin.position.y,origin.position.y+width*resolution),cmap='gray')
-
-        #未知領域を全て描画する場合
-        # self.x_min = origin.position.x
-        # self.x_max = origin.position.x+height*resolution
-        # self.y_min = origin.position.y
-        # self.y_max = origin.position.y+width*resolution
-        # plt.xlim(origin.position.x,origin.position.x+height*resolution)
-        # plt.ylim(origin.position.y,origin.position.y+width*resolution)
-
-        #未知領域を削って描画する場合##################################
-        #地図を描画する際のxyの最大最小値を算出
-        map_data     = np.array([data[i:i+width] for i in range(0, len(data), width)])
-        map_x        = np.sum(map_data, axis=0)
-        map_y        = np.sum(map_data, axis=1)
-        for i in range(0,width,1):
-            if map_x[i] != width*-1:
-                self.x_min_ = i * resolution + origin.position.x
-                break
-        for i in range(width-1,-1,-1):
-            if map_x[i] != width*-1:
-                self.x_max_ = i * resolution + origin.position.x
-                break
-        for i in range(0,height,1):
-            if map_y[i] != height*-1:
-                self.y_min_ = i * resolution + origin.position.y
-                break
-        for i in range(height-1,-1,-1):
-            if map_y[i] != height*-1:
-                self.y_max_ = i * resolution + origin.position.y
-                break
-        #未知領域を描画する幅（m）
-        self.margin = 0.5
-        self.x_min = self.x_min_-self.margin
-        self.x_max = self.x_max_+self.margin
-        self.y_min = self.y_min_-self.margin
-        self.y_max = self.y_max_+self.margin
-        ##########################################################
-
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.xlim(self.x_min,self.x_max)
-        plt.ylim(self.y_min,self.y_max)
-        plt.savefig(data_path + '/' + self.map_file + '/true_' + self.map_file + '.png')
-        plt.savefig(data_path + '/' + self.map_file + '/true_' + self.map_file + '.pdf')
-        plt.cla()
-        plt.clf()
-        plt.close()
-
-        self.map_callback_signal = True
-
-    def map_server(self, request):
-        #map_callback関数で算出した変数を返す。
-        #self.free_array：探索候補点のindexリスト
-        #len(self.free_list)：self.free_listの要素数
-        #self.msg：mapのtopic
-        #self.x_min：地図描画のx軸最小値
-        #self.x_max：地図描画のx軸最大値
-        #self.y_min：地図描画のy軸最小値
-        #self.y_max：地図描画のy軸最大値
-        return em_spcoae_mapResponse(self.free_array, len(self.free_list), self.msg, self.x_min, self.x_max, self.y_min, self.y_max)
-
-    def __init__(self):
-        self.map_file        = rospy.get_param('~map_file')
-        self.map_callback_signal = False
-        self.map_server_signal = False
-
-        rospy.Subscriber('/map_' + self.map_file, OccupancyGrid, self.map_callback, queue_size=1)
-
-        #map_callback関数の処理が終わってからmap_server定義
-        while self.map_server_signal == False:
-            if self.map_callback_signal:
-                s_map = rospy.Service('em_spcoae/map_' + self.map_file, em_spcoae_map, self.map_server)
-                rospy.loginfo('[Server em_spcoae/map]          Ready')
-                self.map_server_signal = True
-
-if __name__ == '__main__':
-    rospy.init_node('em_map_server')
-    hoge = Map()
-    rospy.spin()
-"""
+plt.cla()
+plt.clf()
+plt.close()
